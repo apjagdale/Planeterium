dd805d34983e6515a5b5aabf28268687b18fd3fe
apjagdale <a.jagdale@partner.samsung.com>
2017-05-06 21:46:31 -0400

    Added alpha shader to highlight the planet looking at. Added 360 degree images of Earth and Mars. Click on planet and swipe for 360 images or click to come out of the visited planet

--- planeterium/app/src/main/java/org/gearvrf/planeterium/PlaneteriumActivity.java
+++ planeterium/app/src/main/java/org/gearvrf/planeterium/PlaneteriumActivity.java
@@ -3,13 +3,49 @@ package org.gearvrf.planeterium;
 import org.gearvrf.GVRActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
-public class PlaneteriumActivity extends GVRActivity {
-
+public class PlaneteriumActivity extends GVRActivity implements
+        VRTouchPadGestureDetector.OnTouchPadGestureListener {
+    private VRTouchPadGestureDetector mDetector = null;
+    private PlaneteriumMain planeteriumMainObj = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setMain(new PlaneteriumMain(this));
+        mDetector = new VRTouchPadGestureDetector(this);
+        planeteriumMainObj = new PlaneteriumMain(this);
+        setMain(planeteriumMainObj);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        mDetector.onTouchEvent(event);
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTap(MotionEvent e) {
+        Log.i(TAG, " Yo onTap");
+        planeteriumMainObj.handleTapActivity();
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSwipe(MotionEvent e, VRTouchPadGestureDetector.SwipeDirection swipeDirection, float velocityX, float velocityY) {
+        Log.i(TAG, " Yo onSwipe");
+        planeteriumMainObj.handleSwipeActivity();
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
+        return false;
     }
 }
--- planeterium/app/src/main/java/org/gearvrf/planeterium/PlaneteriumMain.java
+++ planeterium/app/src/main/java/org/gearvrf/planeterium/PlaneteriumMain.java
@@ -9,9 +9,13 @@ import org.gearvrf.GVRMain;
 import org.gearvrf.GVRPicker;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRTexture;
 import org.gearvrf.IPickEvents;
+import org.gearvrf.scene_objects.GVRSphereSceneObject;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
 
 public class PlaneteriumMain extends GVRMain {
 
@@ -24,16 +28,22 @@ public class PlaneteriumMain extends GVRMain {
     public GVRScene mainScene = null;
     public List<Planet> planets = new ArrayList<>();
     private boolean planeNameDisplayed = false;
+    private boolean skyBoxDisplayed = false;
 
     private IPickEvents mPickHandler = new PickHandler();
     private GVRPicker mPicker;
 
+    // For SkyBox
+    private Planet currentPlanet = null;
+    public GVRSceneObject PickedObject = null;
+    private int currentSkyBoxIndex = 0;
+
     public class PickHandler implements IPickEvents
     {
-        public GVRSceneObject   PickedObject = null;
 
         public void onEnter(GVRSceneObject sceneObj, GVRPicker.GVRPickedObject pickInfo) {
             showPlanetName(sceneObj);
+            PickedObject = sceneObj;
         }
         public void onExit(GVRSceneObject sceneObj) {
             removePlanetName(sceneObj);
@@ -93,29 +103,85 @@ public class PlaneteriumMain extends GVRMain {
         createPlanets();
         startRotation();
         showPlanets(mainScene);
+    }
+
+    void handleTapActivity(){
+        if(skyBoxDisplayed){
+            mainScene.removeAllSceneObjects();
+            currentPlanet = null;
+            addHeadTracker(mainScene);
+            showPlanets(mainScene);
+            skyBoxDisplayed = false;
+        }else if(PickedObject != null){
+            for (Planet p:
+                    planets) {
+                if(PickedObject.getName().equals(p.getPlanetName()) && p.getCountOfSkyboxes() != 0){
+                    currentPlanet = p;
+                    currentSkyBoxIndex = -1;
+                    showSkyBox();
+                    skyBoxDisplayed = true;
+                    break;
+                }
+            }
 
 
+        }
+    }
+
+    void handleSwipeActivity(){
+        if(currentPlanet != null)
+            showSkyBox();
+    }
+
+    void showSkyBox(){
+
+        mainScene.removeAllSceneObjects();
+
+        currentSkyBoxIndex++;
+        currentSkyBoxIndex %= currentPlanet.getCountOfSkyboxes();
+
+        GVRSphereSceneObject sphereObject = null;
+        // load texture
+        Future<GVRTexture> texture = null;
+        try {
+            texture = getGVRContext().loadFutureTexture(new GVRAndroidResource(getGVRContext(), currentPlanet.getSkyBoxPath() + "/" + currentPlanet.getSkyBoxWithIndex(currentSkyBoxIndex)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // create a sphere scene object with the specified texture and triangles facing inward (the 'false' argument)
+        sphereObject = new GVRSphereSceneObject(getGVRContext(), false, texture);
+        sphereObject.getTransform().setScale(100, 100, 100);
+
+        mainScene.addSceneObject(sphereObject);
     }
 
     void createPlanets(){
         // Earth;
-        Planet earth = new Planet("Earth", "sphere_earthmap.obj", 560f, getGVRContext());
+        Planet earth = new Planet("Earth", "sphere_earthmap.obj", 560f, getGVRContext(), R.drawable.earthmap1k);
         earth.setPosition(0, 0, -10);
+        earth.setSkyBoxPath("Earth");
+        earth.loadSkyBoxNames(mActivity);
         planets.add(earth);
 
         // Mercury;
-        Planet mercury = new Planet("Mercury", "sphere_mercury.obj", 160f, getGVRContext());
+        Planet mercury = new Planet("Mercury", "sphere_mercury.obj", 160f, getGVRContext(), R.drawable.mercurymap);
         mercury.setPosition(5, 0, -10);
+        mercury.setSkyBoxPath("Mercury");
+        mercury.loadSkyBoxNames(mActivity);
         planets.add(mercury);
 
         // Venus;
-        Planet venus = new Planet("Venus", "sphere_venus.obj", 260f, getGVRContext());
+        Planet venus = new Planet("Venus", "sphere_venus.obj", 260f, getGVRContext(), R.drawable.venusmap);
         venus.setPosition(10, 0, -10);
+        venus.setSkyBoxPath("Venus");
+        venus.loadSkyBoxNames(mActivity);
         planets.add(venus);
 
         // Mars;
-        Planet mars = new Planet("Mars", "sphere_mars.obj", 360f, getGVRContext());
+        Planet mars = new Planet("Mars", "sphere_mars.obj", 360f, getGVRContext(), R.drawable.mars_1k_color);
         mars.setPosition(15, 0, -10);
+        mars.setSkyBoxPath("Mars");
+        mars.loadSkyBoxNames(mActivity);
         planets.add(mars);
     }
 
